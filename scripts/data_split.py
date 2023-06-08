d2169693a7457f72227ea6e4c5604eefe0b5288a
Lingeng <xk21wl@163.com>
2023-06-08 16:26:16 +0800

    finish data split script

--- scripts/data_split.py
+++ scripts/data_split.py
@@ -8,15 +8,15 @@ def split_data(args):
     """
     Split data into 6 : 2 : 2
     """
-    data = pd.read_csv(args.data_path)
+    data = pd.read_csv(args.data_path, index_col=0)
     train_data, test_data = train_test_split(data, test_size=0.4, random_state=args.random_seed)
     test_data, valid_data = train_test_split(test_data, test_size=0.5, random_state=args.random_seed)
     train_data = train_data.rename({'essay': 'text'})
     test_data = test_data.rename({'essay': 'text'})
     valid_data = valid_data.rename({'essay': 'text'})
-    train_data.to_csv(args.train_path)
-    test_data.to_csv(args.test_path)
-    valid_data.to_csv(args.valid_path)
+    train_data.to_csv(args.train_path, index=False)
+    test_data.to_csv(args.test_path, index=False)
+    valid_data.to_csv(args.valid_path, index=False)
 
 
 if __name__ == '__main__':
